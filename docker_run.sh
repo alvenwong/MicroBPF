188871dfd2d03a6563f18a89317a8da267f332c2
ZhuangWang <tmwangzhuang@outlook.com>
2018-10-08 14:12:47 +0000

    write consistent

--- docker_run.sh
+++ docker_run.sh
@@ -2,4 +2,5 @@ sudo docker run -it --rm --privileged \
 	 -v /lib/modules:/lib/modules:ro \
 	 -v /usr/src:/usr/src:ro \
 	 -v /etc/localtime:/etc/localtime:ro \
-	 dockerwangzhuang/bcc:kernel_trace
+	 -v /usr/local:/usr/local:consistent \
+	 dockerwangzhuang/bcc
