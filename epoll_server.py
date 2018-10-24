082130534be677bccdeea59f19f2682936512fe6
zhuangwang93 <tmwangzhuang@outlook.com>
2018-10-24 10:52:47 +0000

    server write to files

--- epoll_server.py
+++ epoll_server.py
@@ -2,6 +2,7 @@
 
 import socket
 import select
+import os
 import errno
 import ConfigParser
 
@@ -9,12 +10,24 @@ import ConfigParser
 config = ConfigParser.RawConfigParser()
 cfgFilename = 'example.cfg'
 
+
 class Server:
-    def __init__(self, host, port):
+    connections = {}
+    messages = {}
+    filefds = {}
+
+    def __init__(self, host, port, path):
         self.host = host
         self.port = port
+        self.path = path
         self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.epoll = select.epoll()
+
+
+    def valid_path(self):
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
 
     def start(self):
         # set SO_REUSEADDR to 1 in this socket
@@ -26,37 +39,49 @@ class Server:
         self.serversocket.setblocking(0)
         self.epoll.register(self.serversocket.fileno(), select.EPOLLIN)
 
+
+    def init_accept(self):
+        connection, address = self.serversocket.accept()
+        print("Connection from: " + str(address))
+        connection.setblocking(0)
+        cfileno = connection.fileno()
+        # Register interest in read (EPOLLIN) events for the new socket.
+        self.epoll.register(cfileno, select.EPOLLIN)
+        self.connections[cfileno] = connection
+        self.filefds[cfileno] = open(self.path+str(address[0])+str(address[1]), "w+")
+
+
+    def close_connection(self, fileno):
+        self.epoll.unregister(fileno)
+        self.connections[fileno].close()
+        del self.connections[fileno]
+        self.filefds[fileno].close()
+
+
+
     def process(self):
-        connections = {}
-        requests = {}
-        responses = {}
         while True:
             events = self.epoll.poll(1)
             for fileno, event in events:
                 if fileno == self.serversocket.fileno():
-                    connection, address = self.serversocket.accept()
-                    print("Connection from: " + str(address))
-                    connection.setblocking(0)
-                    cfileno = connection.fileno()
-                    # Register interest in read (EPOLLIN) events for the new socket.
-                    self.epoll.register(cfileno, select.EPOLLIN)
-                    connections[cfileno] = connection
+                    self.init_accept()
                 elif event & select.EPOLLIN:
                     try:
-                        requests[fileno] = connections[fileno].recv(8)
-                        print(requests[fileno].decode())
+                        self.messages[fileno] = self.connections[fileno].recv(1024)
+                        message = self.messages[fileno].decode()
+                        if len(message) > 0:
+                            print(message)
+                            self.filefds[fileno].write(message)
                         self.epoll.modify(fileno, select.EPOLLIN)
                     except socket.error, e:
                         if isinstance(e.args, tuple):
                             if e[0] == errno.EPIPE:
                                 print("Detected remote disconnect")
-                                self.epoll.unregister(fileno)
-                                connections[fileno].close()
-                                del connections[fileno]
                             else:
                                 print("socket error")
                         else:
                             print("socket error" + e)
+                        self.close_connection(fileno)
                 elif event & select.EPOLLOUT:
                     try:
                         byteswritten = connections[fileno].send(responses[fileno])
@@ -65,18 +90,12 @@ class Server:
                         if isinstance(e.args, tuple):
                             if e[0] == errno.EPIPE:
                                 print("Detected remote disconnect")
-                                self.epoll.unregister(fileno)
-                                connections[fileno].close()
-                                del connections[fileno]
-                                #epoll.modify(fileno, 0)
-                                #connections[fileno].shutdown(socket.SHUT_RDWR)
                         else:
                              print("socket error" + e)
+                        self.close_connection(fileno)
                 # The HUP (hang-up) event indicates that the client socket has been disconnected.
                 elif event & select.EPOLLHUP:
-                    self.epoll.unregister(fileno)
-                    connections[fileno].close()
-                    del connections[fileno]
+                    self.close_connection(fileno)
 
 
     def shutdown(self):
@@ -86,6 +105,7 @@ class Server:
 
 
     def run(self):
+        self.valid_path()
         self.start()
         try:
             self.process()
@@ -99,10 +119,11 @@ def get_parameters():
     config.read(cfgFilename)
     host = config.get("Socket", "IP")
     port = config.get("Socket", "port")
-    return host, int(port)
+    path = config.get("File", "path")
+    return host, int(port), path
 
 
 if __name__ == "__main__":
-    host, port = get_parameters()
-    server = Server(host, port)
+    host, port, path = get_parameters()
+    server = Server(host, port, path)
     server.run()
