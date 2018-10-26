6b7f507f24b2509ae7b90b872fb82ad9301626e7
zhuangwang93 <tmwangzhuang@outlook.com>
2018-10-26 06:32:35 +0000

    add syn

--- clock_test.py
+++
@@ -1,9 +0,0 @@
-
-import time
-
-print(time.clock_gettime(time.CLOCK_REALTIME))
-#print(time.clock_gettime(time.CLOCK_MONOTONIC))
-print(time.time())
-print(time.monotonic())
-print(time.clock_gettime(time.CLOCK_MONOTONIC))
-print(time.clock_gettime(time.CLOCK_MONOTONIC_RAW))
